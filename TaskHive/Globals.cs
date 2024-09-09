da3fb3fe462ccf8e74e2181a89f1a423a856af6e
Cagri Goksu <cagrigoksu.ustundag@gmail.com>
2024-09-09 06:41:02 +0200

    Minor fix on Globals.

--- TaskHive/Globals.cs
+++ TaskHive/Globals.cs
@@ -5,6 +5,5 @@
         public static int UserId = 0;
         public static int HashIter = 5;
         public static int MaxItemForJobList = 10;
-        public static bool Production = false;
     }
 }
